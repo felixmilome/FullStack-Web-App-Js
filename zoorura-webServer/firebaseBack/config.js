995f9b2c2bcad5cfe81997d55733a3323c893f30
felixmilome <75904719+felixmilome@users.noreply.github.com>
2023-10-13 17:05:25 +0300

    Update config.js

--- zoorura-webServer/firebaseBack/config.js
+++ zoorura-webServer/firebaseBack/config.js
@@ -2,13 +2,7 @@ import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
  
 const firebaseConfig = {
-    apiKey: "AIzaSyAR_U3loT-Qmc8-bXjBn6d9N22C9avB5ZU",
-    authDomain: "zooruraweb.firebaseapp.com",
-    projectId: "zooruraweb",
-    storageBucket: "zooruraweb.appspot.com",
-    messagingSenderId: "210073924030",
-    appId: "1:210073924030:web:b036e6fb9763903d2c09ef",
-    measurementId: "G-1SXEYXQM0E"
+   "config Settings"
   };
 
 export const app = initializeApp(firebaseConfig);
