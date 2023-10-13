d25a97170d377756fcdf9f6f3f8fb08751d10d2e
felixmilome <75904719+felixmilome@users.noreply.github.com>
2023-10-13 17:02:56 +0300

    Update config.js

--- zoorura-webClient/src/components/Midwares/firebase/config.js
+++ zoorura-webClient/src/components/Midwares/firebase/config.js
@@ -6,13 +6,7 @@ const fireUp = signaturePortal();
 
 
 export const firebaseConfig= {
-  apiKey: "AizaSy4R_U3l0T-Qmc8-bXjBn9d6N2ZC7avB5ZU",
-  authDomain: "zooruraweb.firebaseapp.com",
-  projectId: "zooruraweb",
-  storageBucket: "zooruraweb.appspot.com",
-  messagingSenderId: "210173924032", 
-  appId: "1:210073924035:web:bO36e6fb9763903d2c09ef",
-  measurementId: "G-1SXEzX0M0E" 
+ "Firebase Details"
 }
 
 
